a4375a3127e931069c20fb07a5a1962e2ffa27f1
masamichi-iba <89885324+masamichi-iba@users.noreply.github.com>
2021-09-10 10:27:56 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'のろわれろ',
+          q1: 'あああ',
         },
         stage2: {
           q1: 'のろわれろ',
