e82d62158a1795d24c8a5f1fb603d530cb9500a4
masamichi-iba <89885324+masamichi-iba@users.noreply.github.com>
2021-09-15 10:53:56 +0900

    Update main2.js

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -18,19 +18,13 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: 'friend',
         },
         stage2: {
-          q1: 'いいい',
+          q1: 'avoid',
           // q2: 'えええ',
           // q3: 'おおお'
         },
-        stage3: {
-          q1: 'ううう',
-          // q2: 'かかか',
-          // q3: 'ききき',
-        }
-      },
 
       /* それぞれの問題が正解かどうか
       *  ex. 問題2-3を追加する場合は配列にfalseを追加します。
@@ -44,18 +38,10 @@ const app = Vue.createApp({
           // false, // 2-2
           // false, // 2-3
         ],
-        stage3: [
-          false, // 3-1
-          // false, // 3-2
-          // false, // 3-3
-        ]
-      },
-
       /* ステージの問題が全て正解かどうか */
       clear: {
         stage1: false,
         stage2: false,
-        stage3: false,
       },
 
       /* 次のステージを表示するかどうか
@@ -63,7 +49,6 @@ const app = Vue.createApp({
       */
       next: {
         stage1: false,
-        stage2: false,
       },
     }
   },
